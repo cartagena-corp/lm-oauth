1641afaed249211a00178e762cd3cb319ad537f6
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2025-03-29 17:59:32 -0500

    Feat: Basic login to test related projects

---
+++ src/main/java/com/cartagenacorp/lm_oauth/entity/User.java
@@ -0,0 +1,22 @@
+package com.cartagenacorp.lm_oauth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
