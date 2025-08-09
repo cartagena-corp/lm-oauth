5a331820ec48234f519adbe8cd973a50aa60895d
Diego-Pedrozo <diegopedrozovillarreal@gmail.com>
2025-08-09 09:59:27 -0500

    build: Generatin .war File instead of .jar

--- src/main/java/com/cartagenacorp/lm_oauth/LmOauthApplication.java
+++ src/main/java/com/cartagenacorp/lm_oauth/LmOauthApplication.java
@@ -2,12 +2,19 @@ package com.cartagenacorp.lm_oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LmOauthApplication {
+public class LmOauthApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmOauthApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LmOauthApplication.class);
 	}
 
 }
